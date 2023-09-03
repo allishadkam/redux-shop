79a716fa9e97b3d8b288b450e27053922b551ac9
ali shadkam <alishadkam007@gmail.com>
2023-09-03 16:56:21 +0430

    add logoc

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import uislice from "./ui-slice";
+
+const store = configureStore({
+    reducer:{ ui : uislice.reducer , }
+});
+
+export default store;
---
+++ src/store/ui-slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uislice = createSlice({
+    name :"ui",
+    initialState:{cartIsvisible:false},
+    reducers:{
+        toggle(state){
+            state.cartIsvisible = !state.cartIsvisible
+        }
+    }
+});
+export const uiActions=uislice.actions;
+export default uislice
