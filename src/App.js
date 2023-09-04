a14c25cb50aef4aaed2a63af082b77710c869900
ali shadkam <alishadkam007@gmail.com>
2023-09-04 14:06:15 +0430

    adding items to backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,11 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector(state=>state.ui.cartIsvisible)
+  const cart = useSelector(state=>state.cart)
+  useEffect(()=>{
+    fetch('https://http-test-5cfcc-default-rtdb.firebaseio.com/productCart.json',{method:"PUT",
+  body:JSON.stringify(cart)});
+  },[cart])
   return (
     <Layout>
       {showCart&&<Cart />}
